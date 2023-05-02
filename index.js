de93edcae77a486d410edbaa0fea332220df990a
Suresh Kumar <suresh05122000@gmail.com>
2023-05-02 15:24:52 +0530

    Added routes.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
 const app = express();
-dotenv.config();
+dotenv.config(); // For accessing data in .env file
 
+/* Connect to mongoDB */
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
@@ -14,6 +19,7 @@ const connect = async () => {
   }
 };
 
+/* Check for connection and output result. */
 mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected.");
 });
@@ -21,6 +27,14 @@ mongoose.connection.on("disconnected", () => {
 mongoose.connection.on("connected", () => {
   console.log("mongoDB connected.");
 });
+
+/* Middlewares. */
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+/* Specify port to listen. */
 app.listen(8800, () => {
   connect();
   console.log("Connected to backend.");
