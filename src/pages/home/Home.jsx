e19360e3db7da078eb1dbbdd4a22a2d6466642b5
Suresh Kumar <suresh05122000@gmail.com>
2023-05-02 14:28:40 +0530

    Added header.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 
 const Home = () => {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
+      <Header />
     </div>
   );
 };
