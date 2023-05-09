778fe415f5df98ed6968c92850b0ba0ed784f87b
Suresh Kumar <suresh05122000@gmail.com>
2023-05-09 09:12:54 +0530

    Added cities list

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+      </div>
     </div>
   );
 };
